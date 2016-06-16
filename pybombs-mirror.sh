bc075548515ae6458493ddec2c59126d5f8b65e2
scateu <scateu@gmail.com>
2016-06-16 16:25:35 +0800

    Shorten PYBOMBS_MIRROR_WORK_DIR

--- pybombs-mirror.sh
+++ pybombs-mirror.sh
@@ -4,6 +4,8 @@ if [ -z $PYBOMBS_MIRROR_WORK_DIR ]; then
 	PYBOMBS_MIRROR_WORK_DIR=$(pwd)
 fi
 
+_DIR=${PYBOMBS_MIRROR_WORK_DIR}
+
 if [ -z $DRY_RUN ]; then
 	DRY_RUN=false  # when set to true, nothing will be fetched, for debug purpose
 fi
@@ -63,42 +65,42 @@ mkdir $1
 #10-retrieve-urls-from-recipes:
 #=============
 
-require_file_existance ${PYBOMBS_MIRROR_WORK_DIR}/upstream-recipe-repos.urls
+require_file_existance ${_DIR}/upstream-recipe-repos.urls
 
-cat ${PYBOMBS_MIRROR_WORK_DIR}/upstream-recipe-repos.urls | while read REPO_URL REPO_NAME
+cat ${_DIR}/upstream-recipe-repos.urls | while read REPO_URL REPO_NAME
 do
-	if [ ! -d ${PYBOMBS_MIRROR_WORK_DIR}/recipes-origin ]; then
-		mkdir ${PYBOMBS_MIRROR_WORK_DIR}/recipes-origin
+	if [ ! -d ${_DIR}/recipes-origin ]; then
+		mkdir ${_DIR}/recipes-origin
 	fi
 
-	if [ ! -d ${PYBOMBS_MIRROR_WORK_DIR}/recipes-origin/${REPO_NAME} ]; then
+	if [ ! -d ${_DIR}/recipes-origin/${REPO_NAME} ]; then
 		echo "No recipes-origin/${REPO_NAME} found. Fetching..."
-		cd ${PYBOMBS_MIRROR_WORK_DIR}/recipes-origin/
+		cd ${_DIR}/recipes-origin/
 		echo "git clone ${REPO_URL} ${REPO_NAME}"
 		git clone ${REPO_URL} ${REPO_NAME}
 	else
 		echo "Git updating recipes..."
-		cd ${PYBOMBS_MIRROR_WORK_DIR}/recipes-origin/${REPO_NAME}
+		cd ${_DIR}/recipes-origin/${REPO_NAME}
 		echo "Updating ${REPO_NAME}"
 		git pull
 	fi
 
-	cd ${PYBOMBS_MIRROR_WORK_DIR}
+	cd ${_DIR}
 done
 
 
-delete_directory_if_exist ${PYBOMBS_MIRROR_WORK_DIR}/_recipes
-cp -r ${PYBOMBS_MIRROR_WORK_DIR}/recipes-origin ${PYBOMBS_MIRROR_WORK_DIR}/_recipes
+delete_directory_if_exist ${_DIR}/_recipes
+cp -r ${_DIR}/recipes-origin ${_DIR}/_recipes
 
-if [ -e ${PYBOMBS_MIRROR_WORK_DIR}/pre-replace-upstream.urls ]; then 
+if [ -e ${_DIR}/pre-replace-upstream.urls ]; then 
 	echo "===================="
 	echo "pre-replace-upstream.urls found!"
 	echo "Patching _recipes with custom urls"
 	
-	cat ${PYBOMBS_MIRROR_WORK_DIR}/pre-replace-upstream.urls | while read origin new
+	cat ${_DIR}/pre-replace-upstream.urls | while read origin new
 	do
 		echo "Replacing ${origin} with ${new}"
-		grep -rl "${origin}" ${PYBOMBS_MIRROR_WORK_DIR}/_recipes/ |grep -v \.git |xargs -r sed -i "s,${origin},${new},g" 
+		grep -rl "${origin}" ${_DIR}/_recipes/ |grep -v \.git |xargs -r sed -i "s,${origin},${new},g" 
 	done
 	echo "Patching _recipes done!"
 	echo "===================="
@@ -107,21 +109,21 @@ fi
 
 
 echo "===================="
-grep  -r -E "(git|svn|wget)\+.*$" ${PYBOMBS_MIRROR_WORK_DIR}/_recipes/ | rev |cut -d' '  -f1 |rev > ${PYBOMBS_MIRROR_WORK_DIR}/recipes-origin.urls
+grep  -r -E "(git|svn|wget)\+.*$" ${_DIR}/_recipes/ | rev |cut -d' '  -f1 |rev > ${_DIR}/recipes-origin.urls
 echo "recipes-origin.urls generated successfully!"
 echo "===================="
 echo 
 
 
-if [ -e ${PYBOMBS_MIRROR_WORK_DIR}/ignore.urls ]; then
+if [ -e ${_DIR}/ignore.urls ]; then
 	echo "===================="
 	echo "ignore.urls found!"
 	echo "Remove ignored urls from recipes-origin.urls"
 	echo "Removed lines won't be fetched or replaced, mirror users can still fetch that repo directly from upstream."
-	cat ${PYBOMBS_MIRROR_WORK_DIR}/ignore.urls | while read url
+	cat ${_DIR}/ignore.urls | while read url
 	do
 		echo "Deleting line contains ${url}"
-		sed -i "\\,${url},d" ${PYBOMBS_MIRROR_WORK_DIR}/recipes-origin.urls 
+		sed -i "\\,${url},d" ${_DIR}/recipes-origin.urls 
 	done
 	echo "recipes-origin.urls patched!"
 	echo "===================="
@@ -130,28 +132,28 @@ fi
 #20-fetch:
 #=============
 
-require_file_existance ${PYBOMBS_MIRROR_WORK_DIR}/recipes-origin.urls
+require_file_existance ${_DIR}/recipes-origin.urls
 
-delete_file_if_exist ${PYBOMBS_MIRROR_WORK_DIR}/_recipes-mirror-replacement.urls 
+delete_file_if_exist ${_DIR}/_recipes-mirror-replacement.urls 
 
-if [ ! -d ${PYBOMBS_MIRROR_WORK_DIR}/git ]; then 
-	mkdir ${PYBOMBS_MIRROR_WORK_DIR}/git
+if [ ! -d ${_DIR}/git ]; then 
+	mkdir ${_DIR}/git
 fi
-if [ ! -d ${PYBOMBS_MIRROR_WORK_DIR}/svn ]; then 
-	mkdir ${PYBOMBS_MIRROR_WORK_DIR}/svn
+if [ ! -d ${_DIR}/svn ]; then 
+	mkdir ${_DIR}/svn
 fi
-if [ ! -d ${PYBOMBS_MIRROR_WORK_DIR}/wget ]; then 
-	mkdir ${PYBOMBS_MIRROR_WORK_DIR}/wget
+if [ ! -d ${_DIR}/wget ]; then 
+	mkdir ${_DIR}/wget
 fi
 
-if [ -e ${PYBOMBS_MIRROR_WORK_DIR}/failed.log ]; then 
+if [ -e ${_DIR}/failed.log ]; then 
 	echo "Previous failed.log found! Roll it to failed.log.1 ."
-	mv ${PYBOMBS_MIRROR_WORK_DIR}/failed.log ${PYBOMBS_MIRROR_WORK_DIR}/failed.log.1
+	mv ${_DIR}/failed.log ${_DIR}/failed.log.1
 	echo
 fi
 
 
-cat ${PYBOMBS_MIRROR_WORK_DIR}/recipes-origin.urls | sed 's/+/ /' | while read protocol url
+cat ${_DIR}/recipes-origin.urls | sed 's/+/ /' | while read protocol url
 do 
 	echo "====================="
 	echo "ENTERING: $PYBOMBS_MIRROR_WORK_DIR"
@@ -173,37 +175,37 @@ do
 
 	FETCHING_SUCCESS=true #predefine status
 	if [ $protocol = "wget" ]; then
-		if [ -e ${PYBOMBS_MIRROR_WORK_DIR}/$TARGET_PATH ]; then
-			echo "${PYBOMBS_MIRROR_WORK_DIR}/$TARGET_PATH exists, skipping"
+		if [ -e ${_DIR}/$TARGET_PATH ]; then
+			echo "${_DIR}/$TARGET_PATH exists, skipping"
 		else
-			if [ -e ${PYBOMBS_MIRROR_WORK_DIR}/${TARGET_PATH}.tmp ]; then
-				echo "${PYBOMBS_MIRROR_WORK_DIR}/${TARGET_PATH}.tmp exists.. remove it."
-				rm ${PYBOMBS_MIRROR_WORK_DIR}/${TARGET_PATH}.tmp 
+			if [ -e ${_DIR}/${TARGET_PATH}.tmp ]; then
+				echo "${_DIR}/${TARGET_PATH}.tmp exists.. remove it."
+				rm ${_DIR}/${TARGET_PATH}.tmp 
 			fi
-			echo "EXECUTING: wget $url -O ${PYBOMBS_MIRROR_WORK_DIR}/${TARGET_PATH}.tmp"
+			echo "EXECUTING: wget $url -O ${_DIR}/${TARGET_PATH}.tmp"
 			if [ ! $DRY_RUN = true ]; then
-				wget $url --tries=3 -O ${PYBOMBS_MIRROR_WORK_DIR}/${TARGET_PATH}.tmp
+				wget $url --tries=3 -O ${_DIR}/${TARGET_PATH}.tmp
 				if [ $? -eq 0 ]; then
-					echo "Fetching done. Renaming ${PYBOMBS_MIRROR_WORK_DIR}/${TARGET_PATH}.tmp to ${PYBOMBS_MIRROR_WORK_DIR}/${TARGET_PATH}"
-					mv ${PYBOMBS_MIRROR_WORK_DIR}/${TARGET_PATH}.tmp ${PYBOMBS_MIRROR_WORK_DIR}/${TARGET_PATH}
+					echo "Fetching done. Renaming ${_DIR}/${TARGET_PATH}.tmp to ${_DIR}/${TARGET_PATH}"
+					mv ${_DIR}/${TARGET_PATH}.tmp ${_DIR}/${TARGET_PATH}
 				else
 					FETCHING_SUCCESS=false
 				fi
 			fi
 		fi
 	elif [ $protocol = "git" ]; then
-		if [ -d ${PYBOMBS_MIRROR_WORK_DIR}/$TARGET_PATH ]; then
-			echo "${PYBOMBS_MIRROR_WORK_DIR}/$TARGET_PATH exists, syncing"
-			cd ${PYBOMBS_MIRROR_WORK_DIR}/$TARGET_PATH
+		if [ -d ${_DIR}/$TARGET_PATH ]; then
+			echo "${_DIR}/$TARGET_PATH exists, syncing"
+			cd ${_DIR}/$TARGET_PATH
 			
 			if [ ! $DRY_RUN = true ]; then
 				/usr/bin/timeout -s INT 3600 git remote -v update || FETCHING_SUCCESS=false
 				git repack -a -d
 			fi
 		else
-			echo "EXECUTING: git clone --mirror $url ${PYBOMBS_MIRROR_WORK_DIR}/$TARGET_PATH"
+			echo "EXECUTING: git clone --mirror $url ${_DIR}/$TARGET_PATH"
 			if [ ! $DRY_RUN = true ]; then
-				git clone --mirror $url ${PYBOMBS_MIRROR_WORK_DIR}/$TARGET_PATH || FETCHING_SUCCESS=false
+				git clone --mirror $url ${_DIR}/$TARGET_PATH || FETCHING_SUCCESS=false
 			fi
 		fi
 	elif [ $protocol = "svn" ]; then
@@ -217,7 +219,7 @@ do
 
 	cd $PYBOMBS_MIRROR_WORK_DIR
 	if [ $FETCHING_SUCCESS = true ]; then
-		echo "${ORIGIN_PYBOMBS_URL} ${MIRROR_PYBOMBS_URL}" >> ${PYBOMBS_MIRROR_WORK_DIR}/_recipes-mirror-replacement.urls
+		echo "${ORIGIN_PYBOMBS_URL} ${MIRROR_PYBOMBS_URL}" >> ${_DIR}/_recipes-mirror-replacement.urls
 		echo "${ORIGIN_PYBOMBS_URL} fetching success!"
 	else
 		echo "${ORIGIN_PYBOMBS_URL}" >> failed.log
@@ -232,50 +234,50 @@ done
 #30-replace-_recipes:
 #=============
 
-require_file_existance ${PYBOMBS_MIRROR_WORK_DIR}/_recipes-mirror-replacement.urls 
-require_directory_existance ${PYBOMBS_MIRROR_WORK_DIR}/_recipes
+require_file_existance ${_DIR}/_recipes-mirror-replacement.urls 
+require_directory_existance ${_DIR}/_recipes
 
 echo "Replacing PYBOMBS_MIRROR_BASE_URL with ${PYBOMBS_MIRROR_BASE_URL} in _recipes-mirror-replacement.urls .."
-cp ${PYBOMBS_MIRROR_WORK_DIR}/_recipes-mirror-replacement.urls ${PYBOMBS_MIRROR_WORK_DIR}/_recipes-mirror-replacement.urls 
+cp ${_DIR}/_recipes-mirror-replacement.urls ${_DIR}/_recipes-mirror-replacement.urls 
 sed -i "s,PYBOMBS_MIRROR_BASE_URL,${PYBOMBS_MIRROR_BASE_URL},g" _recipes-mirror-replacement.urls
 echo "Done."
 
 
-cat ${PYBOMBS_MIRROR_WORK_DIR}/_recipes-mirror-replacement.urls | while read origin new
+cat ${_DIR}/_recipes-mirror-replacement.urls | while read origin new
 do
 	echo "${origin} --> ${new}"
-	grep -rl "${origin}" ${PYBOMBS_MIRROR_WORK_DIR}/_recipes/ |grep -v \.git |xargs -r sed -i "s,${origin},${new},g" 
+	grep -rl "${origin}" ${_DIR}/_recipes/ |grep -v \.git |xargs -r sed -i "s,${origin},${new},g" 
 	echo
 done
 
 #40-deploy:
 #=============
 
-require_file_existance ${PYBOMBS_MIRROR_WORK_DIR}/upstream-recipe-repos.urls 
-require_directory_existance ${PYBOMBS_MIRROR_WORK_DIR}/_recipes 
+require_file_existance ${_DIR}/upstream-recipe-repos.urls 
+require_directory_existance ${_DIR}/_recipes 
 
 ## Make .git bare repos for recipes.
 
-make_clean_dir ${PYBOMBS_MIRROR_WORK_DIR}/recipes
+make_clean_dir ${_DIR}/recipes
 
-cat ${PYBOMBS_MIRROR_WORK_DIR}/upstream-recipe-repos.urls | while read REPO_URL REPO_NAME
+cat ${_DIR}/upstream-recipe-repos.urls | while read REPO_URL REPO_NAME
 do
-	if [ -e ${PYBOMBS_MIRROR_WORK_DIR}/_recipes/${REPO_NAME} ]; then
-		cd ${PYBOMBS_MIRROR_WORK_DIR}/_recipes/${REPO_NAME};
+	if [ -e ${_DIR}/_recipes/${REPO_NAME} ]; then
+		cd ${_DIR}/_recipes/${REPO_NAME};
 		git commit -am "PyBOMBS Mirror Replacement: $(date)"
 	else
 		echo "_recipes/${REPO_NAME} doesn't exist!"
 	fi
 	
-	git clone --mirror ${PYBOMBS_MIRROR_WORK_DIR}/_recipes/${REPO_NAME} ${PYBOMBS_MIRROR_WORK_DIR}/recipes/${REPO_NAME}.git
+	git clone --mirror ${_DIR}/_recipes/${REPO_NAME} ${_DIR}/recipes/${REPO_NAME}.git
 done
 
 echo "Cleaning up"
 
-delete_file_if_exist ${PYBOMBS_MIRROR_WORK_DIR}/_recipes-mirror-replacement.urls
-delete_directory_if_exist ${PYBOMBS_MIRROR_WORK_DIR}/_recipes
+delete_file_if_exist ${_DIR}/_recipes-mirror-replacement.urls
+delete_directory_if_exist ${_DIR}/_recipes
 
 
 echo "maybe you need to : sudo chown -R www-data:www-data ${PYBOMBS_SITE_BASE}"
 
-cd ${PYBOMBS_MIRROR_WORK_DIR}
+cd ${_DIR}
